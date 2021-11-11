1d45ed0921d83024b91759bc0a6b0d6195786ac9
Rakibul-Hasan-max <rakibulhasan4041@gmail.com>
2021-11-11 18:17:30 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const { MongoClient } = require('mongodb');
+require('dotenv').config();
+const cors = require('cors');
+const ObjectId = require('mongodb').ObjectId;
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.p5r6u.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+console.log(uri);
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
+
+app.get('/', (req, res) => {
+	res.send('Running Dreamland server');
+})
+
+app.listen(port, () => {
+	console.log('Listening Dreamland server on port', port);
+})
