35405731645fe6f7d0140c984839996c068b485c
Rakibul-Hasan-max <rakibulhasan4041@gmail.com>
2021-11-13 21:22:53 +0600

    admin

--- index.js
+++ index.js
@@ -79,6 +79,16 @@ async function run(){
             res.json(property);
         })
         
+        app.put('/users/admin', async (req, res) => {
+            const user = req.body;
+            console.log('put', user);
+            const filter = { email: user.email };
+            const updateDoc = { $set: { role: 'admin' } };
+            const result = await usersCollection.updateOne(filter, updateDoc);
+            res.json(result);
+        });
+
+
         // DELETE API
         // app.delete('/services/:id', async (req, res) => {
         //     const id = req.params.id;
